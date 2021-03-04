72307434c07204886e7683c274273421b72b75b6
Ciaran <44727609+ShyguyCC@users.noreply.github.com>
2021-03-04 10:49:40 +0000

    Objects and functions for tests on exersize 2 are setup now

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,12 +2,12 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
+    public boolean isFromManchester(Person person) {
         // Add your code here
         return false;
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
+    public boolean canWatchFilm(Person person, int ageLimit) {
         // Add your code here
         return false;
     }
