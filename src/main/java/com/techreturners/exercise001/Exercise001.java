6077d4fe6e61e16c67332801e5a753b509fd00d4
Ciaran <ShyguyCC@users.noreply.github.com>
2021-03-17 08:26:49 +0000

    Refactored functions reverse and countLinuxUsers

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -21,20 +21,18 @@ public class Exercise001 {
     public double addVat(double originalPrice, double vatRate) {
         double Vat = originalPrice + ((vatRate / 100) * originalPrice);
         //this rounds up to 2 decimal places, number of zeros in round funtion = how many decimal places.
-        Vat = (double)Math.round(temp * 100d) / 100d;
+        Vat = (double)Math.round(Vat * 100d) / 100d;
         return Vat;
     }
 
     public String reverse(String sentence) {
         //unable to put this stright into a return as it needs to become a string first then we can return the answer.
         //we now are able to use string buffer to break the sentence down, then reverse and then put it back into a string and put that answer to Answer.
-        String ReversedWord = new StringBuffer(sentence).reverse().toString();
-        return ReversedWord;
+        return new StringBuffer(sentence).reverse().toString();
     }
 
     public int countLinuxUsers(List<User> users) {
         //here we use the filter function to go through the users array and get any that have a getType of linux, then add that to a temp array. we then return the size of that array as thats how many times we have a true to the if statement within the filter.
-        List<User> linuxUserList = users.stream().filter(u -> u.getType().equals("Linux")).collect(Collectors.toList());
-        return linuxUserList.size();
+        return users.stream().filter(u -> u.getType().equals("Linux")).collect(Collectors.toList()).size();
     }
 }
